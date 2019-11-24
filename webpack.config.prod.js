82be2cbfec4de163e49d1e5e89e604ca1764a0c0
Stephane Cottereau <stefan.cottereau@gmail.com>
2019-11-24 18:35:24 +0000

    Added bundle analyzer feature from production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,17 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
+// use curly brackets to avoid BundleAnalyzerPlugin is not a constructor error
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   // build: slowest, rebuild: slower, production: yes, quality: original source
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'bundle_analyzer.html'
+      })
+  ]
 })
