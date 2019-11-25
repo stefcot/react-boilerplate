527611f3cea7e245b8e3c79ddaeaf7436bb8c9c9
Stephane Cottereau <stefan.cottereau@gmail.com>
2019-11-25 17:16:04 +0000

    Added eslint accessibility check plugin

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const Warning = React.lazy(() => import('./warning'))
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     // This binding is necessary to make `this` work in the callback
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
   }
 
   // have to install @babel/plugin-proposal-class-properties
@@ -18,41 +18,44 @@ class App extends React.Component {
     count: 0
   }
 
-  setCountTo12()  {
+  setCountTo12() {
     this.setState({
-      count: 12,
-    });
+      count: 12
+    })
   }
 
   increment() {
-    this.setState({count: this.state.count + 1})
+    this.setState({ count: this.state.count + 1 })
   }
 
-  decrement(){
-    this.setState({count: this.state.count - 1})
+  decrement() {
+    this.setState({ count: this.state.count - 1 })
   }
 
   render() {
-    const {count} = this.state
+    const { count } = this.state
     return (
-        <div>
-          <h1>Added watch mode to webpack config!</h1>
-          <p>We have to refresh the browser though.</p>
-          <h2>Served by webpack dev server</h2>
-          <p>Now all modifications will be watched by webpack-dev-server.<br/>
-            <em>All will be built ad watched automatically</em> with: <br/>
-            <code>webpack-dev-server --open --config webpack.config.dev.js</code>
-          </p>
-          <h2>State</h2>
-          <p className={count >10 ? 'warning' : null}>Count: {count}</p>
-          <button onClick={this.increment}>+</button>
-          <button onClick={this.decrement}>-</button>
-          {count >10 ?
-            <React.Suspense fallback={null}>
-              <Warning/>
-            </React.Suspense>
-            : null}
-        </div>
+      <div>
+        <img src="./logo.png" alt="" />
+        <h1>Added watch mode to webpack config!</h1>
+        <p>We have to refresh the browser though.</p>
+        <h2>Served by webpack dev server</h2>
+        <p>
+          Now all modifications will be watched by webpack-dev-server.
+          <br />
+          <em>All will be built ad watched automatically</em> with: <br />
+          <code>webpack-dev-server --open --config webpack.config.dev.js</code>
+        </p>
+        <h2>State</h2>
+        <p className={count > 10 ? 'warning' : null}>Count: {count}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
     )
   }
 }
