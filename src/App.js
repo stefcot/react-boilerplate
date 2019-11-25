25f0617bd9a0f7a47717fc67b0530e6af2158aab
Stephane Cottereau <stefan.cottereau@gmail.com>
2019-11-25 18:49:46 +0000

    Code clean up for new projects use

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
 import React from 'react'
-//import Warning from './warning'
-// with this version of babel, don't need to add @babel/plugin-syntax-dynamic-import
-
-// Will split this piece of code
-const Warning = React.lazy(() => import('./warning'))
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    // This binding is necessary to make `this` work in the callback
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
-
-  // have to install @babel/plugin-proposal-class-properties
-  state = {
-    count: 0
-  }
-
-  setCountTo12() {
-    this.setState({
-      count: 12
-    })
-  }
-
-  increment() {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - 1 })
-  }
-
   render() {
-    const { count } = this.state
     return (
       <main>
-        <img src="./logo.png" alt="" />
-        <h1>Added watch mode to webpack config!</h1>
-        <h2>
-          Subtitle gonna raise an accessibility issue with React axe if{' '}
-          <code>&lt;h3&gt;</code> is used
-        </h2>
-        <p>We have to refresh the browser though.</p>
-        <h2>Served by webpack dev server</h2>
-        <p>
-          Now all modifications will be watched by webpack-dev-server.
-          <br />
-          <em>All will be built ad watched automatically</em> with: <br />
-          <code>webpack-dev-server --open --config webpack.config.dev.js</code>
-        </p>
-        <h2>State</h2>
-        <p className={count > 10 ? 'warning' : null}>Count: {count}</p>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
+        <h1>Base for your project</h1>
       </main>
     )
   }
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,35 +1,28 @@
 // Initialize a set of tests
 import React from 'react'
-import { render, fireEvent, cleanup } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import App from './App'
 
 const setup = () => {
-  const {container, getByText, asFragment } = render(<App/>)
-  return {container, getByText, asFragment }
+  const { container, getByText, asFragment } = render(<App />)
+  return { container, getByText, asFragment }
 }
 
 describe('App', () => {
-  afterEach(cleanup);
+  afterEach(cleanup)
   // Supposed to fail cause of the lazy loading,
   // Otherwise, install babel-plugin-dynamic-import-node
   // cause jest runs on node
   it('It should render App component', () => {
-    render(<App/>)
+    render(<App />)
   })
 
-  it('It should render App component with count set to 12', () => {
-    const { getByText, asFragment } = setup()
-    const timesToClick = 12
-    const addButton = getByText('+')
+  it('It should render App component the same as snapshot', () => {
+    const { asFragment } = setup()
 
-    for(let i = 0; i <=timesToClick; i = i + 1) {
-      fireEvent.click(addButton)
-    }
-
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot()
 
     // For state testing and others functionnal assertions
     // It's better to use enzyme
-    // expect(getByText(container, timesToClick)).toBeVisible()
   })
 })
