a3ab4b7ff3715942ded0f0f545ea043eb5223fb9
Stephane Cottereau <stefan.cottereau@gmail.com>
2019-11-25 16:33:31 +0000

    Added prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import {hot} from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
 
-if(hot){
+if (hot) {
   hot(module)('./App', () => {
-    const NextApp = require('./App').default;
-    ReactDOM.render(
-        <NextApp />,
-        document.getElementById('root')
-    );
-  });
+    const NextApp = require('./App').default
+    ReactDOM.render(<NextApp />, document.getElementById('root'))
+  })
 }
